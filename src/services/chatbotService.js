7f83634d0eea69691d59e6b9909d73fe61f7d73c
Dang Duc Chinh <chinh22082001@gmail.com>
2024-05-28 14:50:36 +0700

    button

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 import request from "request";
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const IMAGE_GET_STARTED = "https://hinhdep.khangviet.net/wp-content/uploads/2020/02/Banner-website-ph%C3%B2ng-kh%C3%A1m-website-b%E1%BB%87nh-vi%E1%BB%87n-ng%C3%A0nh-y-t%E1%BA%BF.jpg?v=1582777556";
 let callSendAPI = (sender_psid, response) => {
     // Construct the message body
     let request_body = {
@@ -46,14 +47,57 @@ let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
             let username = await getUserName(sender_psid);
-            let response = { "text": `OK , xin chào mừng bạn ${username} đến với chatbot của chúng tôi` };
-            await callSendAPI(sender_psid, response);   
+            // text giới thieu khi get started
+            let response1 = { "text": `OK , xin chào mừng bạn ${username} đến với chatbot của chúng tôi` };
+
+            //generic template
+            let response2 = await sendGetStartedTemplate();
+
+            // gui 2 tin nhan den cho nguoi dung
+            await callSendAPI(sender_psid, response1);   
+            await callSendAPI(sender_psid, response2);   
+
             resolve('done');
         } catch (error) {
             reject(error);
         }
     })
 }
+
+let sendGetStartedTemplate = ()=>{
+    let response = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Xin chào bạn đến với chatbot bookingcare!",
+                    "subtitle": "Dưới đây là các lựa chọn: ",
+                    "image_url": IMAGE_GET_STARTED,
+                    "buttons": [
+                        {
+                            "type": "postback",
+                            "title": "MENU CHÍNH",
+                            "payload": "MAIN_MENU",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "CHUYÊN KHOA Y TẾ",
+                            "payload": "SPECIALTIES",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "HƯỚNG DẪN SỬ DỤNG BOT",
+                            "payload": "GUIDE_TO_USE",
+                        }
+                    ],
+                }]
+            }
+        }
+    }
+    return response ; 
+}
+
 
 module.exports = {
     handleGetStarted: handleGetStarted
